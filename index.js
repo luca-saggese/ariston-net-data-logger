e0b1819198b962c0c91c72e86a7ca2b4b296d886
Luca Saggese <luca.saggese@gmail.com>
2022-05-21 14:26:21 +0200

    wip

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const AristonApi = require("ariston-remotethermo-client");
-const credentails = require ('./credentials.json')
+const moment = require("moment-timezone");
+const fs = require('fs')
+const credentails = require ('./credentials.json');
+
+
+
+const path = __dirname + '/out.csv';
+
 const ariston = new AristonApi(credentails.username, credentails.password, credentails.heater_id);
+
+if (!fs.existsSync(path)) {
+    fs.appendFileSync(path, 'Date;OutTemp;InTemp\r\n');
+}
+
 
 ariston.login().then(() => {
   ariston.getStatus().then((params) => {
+    const date = moment(new Date(),'Europe/Rome')
+    console.log(date.format('DD/MM/yyyy HH:mm:ss'));
     console.log("Comfort Temperature:", params.zone.comfortTemp.value);
     console.log("Outdoor Temperature:", params.outsideTemp);
     console.log("Room Temperature:", params.zone.roomTemp);
-    ariston.getComfortStatus().then((value) => {
-      console.log("Comfort mode:", value);
-      ariston.setComfortStatus(3).then((newState) => {
-        console.log("Comfort mode:", newState);
-      });
-    });
+    fs.appendFileSync(path, date.format('DD/MM/yyyy HH:mm:ss') + ';'+ params.outsideTemp +';'+ params.zone.roomTemp +'\r\n');
+
+    process.exit(0)
+
+    // ariston.getComfortStatus().then((value) => {
+    //   console.log("Comfort mode:", value);
+    //   ariston.setComfortStatus(3).then((newState) => {
+    //     console.log("Comfort mode:", newState);
+    //   });
+    // });
 
   });
 });
